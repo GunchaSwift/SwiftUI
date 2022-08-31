13d281fbaf8261ff7918653aa8b9bb14ca2872a4
GunchaPy <guntars.reiss@gmail.com>
2022-08-31 17:57:19 +0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Guntars Reiss on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
